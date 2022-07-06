e9e7c1a483f44715d52a90a168c5d48c7cf85bba
titisata <titaya@knowledge-cs.com>
2022-07-06 13:52:01 +0900

    henkou

--- resources/views/experience/detail.blade.php
+++ resources/views/experience/detail.blade.php
@@ -91,10 +91,10 @@ async function commentCreate(ex_id) {
                         <p>この体験はご利用できません</p>
                     @endforelse
                 </div> -->
-                <div class="card-body d-flex flex-column">
+                <div class="card-body">
                     @forelse($experiences as $experience)
                         
-                     <a class="btn btn-primary w-50 my-1" href="{{ $experienceFolder->id }}/{{ $experience->id }}?{{ explode('?', str_replace(url('/'),"",request()->fullUrl()))[1] }}">{{ $experience->name }}</a>
+                     <a class="btn btn-primary  my-1" href="{{ $experienceFolder->id }}/{{ $experience->id }}?{{ explode('?', str_replace(url('/'),"",request()->fullUrl()))[1] }}">{{ $experience->name }}</a>
                                                             
                     @empty
                         <p>この体験はご利用できません</p>
