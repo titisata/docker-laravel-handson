28e6db8ba0589d499a2772168464dd33ed462143
titisata <titaya@knowledge-cs.com>
2022-07-06 13:43:29 +0900

    henkou

--- resources/views/experience/detail.blade.php
+++ resources/views/experience/detail.blade.php
@@ -94,10 +94,10 @@ async function commentCreate(ex_id) {
                 <div class="card-body">
                     @forelse($experiences as $experience)
                         <div class="mt-1 p-3 card">
-                            <div>
+                            
                             <a class="btn btn-primary" href="{{ $experienceFolder->id }}/{{ $experience->id }}?{{ explode('?', str_replace(url('/'),"",request()->fullUrl()))[1] }}">{{ $experience->name }}</a>
-                                <p>大人: {{ $experience->price_adult }}円 子ども: {{ $experience->price_child }}円</p>                      
-                            </div>
+                            <p>大人: {{ $experience->price_adult }}円 子ども: {{ $experience->price_child }}円</p>                      
+                            
                         </div>
                     @empty
                         <p>この体験はご利用できません</p>
