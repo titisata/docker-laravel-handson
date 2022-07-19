4957668bb4e011fafc3bc1952045c4254b1d80e3
titisata <titaya@knowledge-cs.com>
2022-07-19 17:24:31 +0900

    henkou

--- resources/views/components/experience_cell.blade.php
+++ resources/views/components/experience_cell.blade.php
@@ -1,25 +1,30 @@
 <style>
+ *{
+    box-sizing:content-box;
+}
 .card-text {
     word-wrap: break-word;
 }
 </style>
 <meta name="csrf-token" content="{{ csrf_token() }}">
-
-<div class="card mb-3 width: 100%;">
-    <div style="display: flex; flex: 1 1 auto; height: 200px; width: 100%;">
-        <a href="/experience/{{ $experienceFolder->id }}?{{ explode('?', str_replace(url('/'),"",request()->fullUrl()))[1] }}" style="text-decoration: none; color: inherit;">
-            <div style="display: flex; flex: 1 1 auto; height: 100%;">
-                <div class="img-square-wrapper">
-                    <img style="object-fit: cover; height: 100%;" class="" src="{{ $experienceFolder->images()[0]?->image_path ?? '/images/empty.png'}}" alt="Card image cap" width="200">
+<li class="item mx-3">
+    <div class="card shadow mb-3 rounded-3" style="max-width:300px;">
+        <div style="display: flex;max-width:270px; ">
+            <a href="/experience/{{ $experienceFolder->id }}?{{ explode('?', str_replace(url('/'),"",request()->fullUrl()))[1] }}" style="text-decoration: none; color: inherit;">
+                <div style="display: flex; flex: 1 1 auto; height: 100%;max-width:270px; flex-direction:column;">
+                    <div class="img-square-wrapper">
+                        <img style="object-fit: cover; width:270px;height: 180px;" class="rounded-top" src="{{ $experienceFolder->images()[0]?->image_path ?? '/images/empty.png'}}" alt="Card image cap" width="270">
+                    </div>
+                    <div class="card-header border-none" style="background:white;border-bottom:none">
+                        <h5 class="card-title text-center mb-0 text-truncate" >{{ $experienceFolder->name }}</h5>
+                    </div>
+                    <div class="card-body">
+                        
+                        <p class="card-text text-end fw-bold fs-4">￥{{ $experienceFolder->price }}～</p>
+                        <p class="card-text">{{ $experienceFolder->is_lodging ? ('宿泊日: ' . ($experienceFolder->is_before_lodging ?  (new DateTime(app('request')->input('keyword')))->modify("-1day")->format('Y-m-d') . ' (前泊)' : app('request')->input('keyword') . ' (後泊)') ) : '宿泊なし' }}</p>
+                    </div>
                 </div>
-                <div class="card-body" style="width: 1000px;">
-                    <h4 class="card-title" style="text-overflow: ellipsis; white-space: nowrap; overflow: hidden; width: 60%;">{{ $experienceFolder->name }}</h4>
-                    <p class="card-text" style="text-overflow: ellipsis; white-space: nowrap; overflow: hidden; width: 60%;">{{ $experienceFolder->description }}</p>
-                    <p class="card-text">￥{{ $experienceFolder->price }}～</p>
-                    <p>開催日: {{ app('request')->input('keyword') }}</p>
-                    <p class="card-text">{{ $experienceFolder->is_lodging ? ('宿泊日: ' . ($experienceFolder->is_before_lodging ?  (new DateTime(app('request')->input('keyword')))->modify("-1day")->format('Y-m-d') . ' (前泊)' : app('request')->input('keyword') . ' (後泊)') ) : '宿泊なし' }}</p>
-                </div>
-            </div>
-        </a>
+            </a>
+        </div>
     </div>
-</div>
+</li>
--- resources/views/search/experience_list.blade.php
+++ resources/views/search/experience_list.blade.php
@@ -1,9 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
+<style>
+    ul.horizontal-list {
+	overflow-x: auto;
+	white-space: nowrap;
+}
+li.item {
+	display: inline-block;
+}	
+</style>
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10">
 
             <h3 class="mb-3">体験検索結果</h3>
 
@@ -12,13 +21,18 @@
                 <input type="submit" value="検索">
             </form>
 
-            @forelse($experienceFolders as $experienceFolder)
-                @if (!$experienceFolder->is_holiday(app('request')->input('keyword')))
-                    @include('components.experience_cell', ['experienceFolder'=>$experienceFolder])
-                @endif
-            @empty
-                <p>検索結果がありません。</p>
-            @endforelse
+            <h2 class="mt-5 mb-4 ms-3 ms-md-0 fw-bold">おすすめの体験</h2>
+            <div class="d-flex justify-content-evenly ">
+                <ul class="horizontal-list">
+                @forelse($experienceFolders as $experienceFolder)
+                    @if (!$experienceFolder->is_holiday(app('request')->input('keyword')))
+                        @include('components.experience_cell', ['experienceFolder'=>$experienceFolder])
+                    @endif
+                @empty
+                    <p>検索結果がありません。</p>
+                @endforelse
+                </ul>
+            </div>
             <!-- <a href="/search/goods">土産検索へ</a> -->
         </div>
     </div>
