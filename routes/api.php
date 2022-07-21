64d50985e3dcf84be2a25725e76164212cad7e41
kobayashiharuto <datesaver1234@gmail.com>
2022-07-21 14:17:30 +0900

    カートから削除する処理追加

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/comment/experience', [App\Http\Controllers\CommentsController::class, 'experience_post']);
     Route::post('/comment/goods', [App\Http\Controllers\CommentsController::class, 'goods_post']);
-    Route::delete('/cart/experience', [App\Http\Controllers\ExperienceController::class, 'delete_experience']);
-    Route::delete('/cart/goods', [App\Http\Controllers\ExperienceController::class, 'delete_goods']);
+    Route::delete('/cart/experience', [App\Http\Controllers\CartController::class, 'delete_experience']);
+    Route::delete('/cart/goods', [App\Http\Controllers\CartController::class, 'delete_goods']);
 });
