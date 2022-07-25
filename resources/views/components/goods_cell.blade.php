ebf1286c8564da44d7f3e1200a86cf9255a23c54
titisata <titaya@knowledge-cs.com>
2022-07-25 10:10:43 +0900

    henkou

--- resources/views/components/goods_cell.blade.php
+++ resources/views/components/goods_cell.blade.php
@@ -1,24 +1,34 @@
 <style>
+*{
+    box-sizing:content-box;
+}
 .card-text {
     word-wrap: break-word;
 }
 </style>
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
-<div class="card mb-3 width: 100%;">
-    <div style="display: flex; flex: 1 1 auto; height: 200px; width: 100%;">
-        <a href="/goods/{{ $goods_folder->id }}" style="text-decoration: none; color: inherit;">
-            <div style="display: flex; flex: 1 1 auto; height: 100%;">
-                <div class="img-square-wrapper">
-                    <img style="object-fit: cover; height: 100%;" class="" src="{{ $goods_folder->images()[0]?->image_path ?? '/images/empty.png'}}" alt="Card image cap" width="200">
+<li class="item mx-3">
+    <div  class="card shadow mb-3 rounded-3" style="max-width:300px;">
+        <div  style="display: flex;max-width:270px; ">
+            <a href="/goods/{{ $goods_folder->id }}" style="text-decoration: none; color: inherit;">
+                <div style="display: flex; flex: 1 1 auto; height: 100%; max-width:270px; flex-direction:column;"">
+                    <div class="img-square-wrapper">
+                        <img style="object-fit: cover; width:270px;height: 180px;" class="rounded-top" src="{{ $goods_folder->images()[0]?->image_path ?? '/images/empty.png'}}" alt="Card image cap" width="270">
+                    </div>
+                    <div class="card-header border-none" style="background:white;border-bottom:none">
+                        <h4 class="card-title text-center mb-0 text-truncate" >{{ $goods_folder->name }}</h4>
+                    </div>
+                    <div class="card-body">
+                        
+                        <p class="card-text">￥{{ $goods_folder->price }}</p>
+                    </div>
                 </div>
-                <div class="card-body" style="width: 1000px;">
-                    <h4 class="card-title" style="text-overflow: ellipsis; white-space: nowrap; overflow: hidden; width: 60%;">{{ $goods_folder->name }}</h4>
-                    <p class="card-text" style="text-overflow: ellipsis; white-space: nowrap; overflow: hidden; width: 60%;">{{ $goods_folder->description }}</p>
-                    <p class="card-text">￥{{ $goods_folder->price }}</p>
-                </div>
-            </div>
-        </a>
+            </a>
+        </div>
     </div>
-</div>
+</li>
+
+
+
 
--- resources/views/search/goods_list.blade.php
+++ resources/views/search/goods_list.blade.php
@@ -1,6 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
+<style>
+    ul.horizontal-list {
+	overflow-x: auto;
+	white-space: nowrap;
+}
+li.item {
+	display: inline-block;
+}
+</style>
 <div class="container">
     <div class="row justify-content-center">
         <div class="ms-3 ms-md-0 mt-4">
@@ -11,7 +20,7 @@
                     @csrf
                     <div class="d-flex flex-column align-items-center">
                         <input name="keyword" type="text" class="form-control" style="width:240px" placefolder="検索したいお土産を入力してください" value="{{ app('request')->input('keyword') }}">
-                        <select name="category" class="form-select mt-2" style="width:240px;">
+                        <select name="category" class="form-select mt-2" style="width:216px;">
                             <option value="">カテゴリ選択</option>
                             @foreach ($categories as $category)
                                 <option value="{{ $category->name }}">{{ $category->name }}</option>
@@ -23,12 +32,17 @@
             </div>
         </div>
 
-            @forelse($goods_folders as $goods_folder)
-                @include('components.goods_cell', ['goods_folder'=>$goods_folder])
-            @empty
-                <p>検索結果がありません。</p>
-            @endforelse
-            <a href="/search/experience">体験検索へ</a>
+        <h2 class="mt-5 mb-4 ms-3 ms-md-0 fw-bold">検索結果</h2>
+        <div class="d-flex justify-content-evenly ">
+            <ul class="horizontal-list">
+                @forelse($goods_folders as $goods_folder)
+                    @include('components.goods_cell', ['goods_folder'=>$goods_folder])
+                @empty
+                    <p>検索結果がありません。</p>
+                @endforelse
+            </ul>
+        </div>
+            
     
         <div class="d-flex justify-content-center mt-3">
             <div>{{ $goods_folders->appends(request()->query())->links('pagination::bootstrap-4') }}</div>
